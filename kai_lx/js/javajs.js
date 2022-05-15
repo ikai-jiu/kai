418735fab0047f41a85f849c88218a2b45520b5b
ikai <3540559834@qq.com>
2022-05-15 15:52:51 +0800

    kai

--- kai_lx/js/javajs.js
+++ kai_lx/js/javajs.js
@@ -1,5 +1,16 @@
-async function test() {
-    // 调用python函数
-    const res = await eel.my_add(3, 4)();
-    console.log(res);
+function zTreeOnRename(event, treeId, treeNode, isCancel) {
+    // alert(treeNode.tId + ", " + treeNode.name);
+    $.ajax({
+        url: "../python/pyexe.py",
+        type: "get",
+        dataType: "text",
+        // data: {dt: $("#dt").val(), tm: $("#tm").val() },
+        success: textSucceed,
+        error: Error
+    });
+    // .done(function( o ) {
+    // // do something
+    // });
+    // process = fn.zTree.Process(vr=treeNode.name,is_template= true, status="aaa");
+    // process.save();
 }
--- kai_lx/python/pyexe.py
+++ kai_lx/python/pyexe.py
@@ -1,6 +1,18 @@
-import eel
+class Process(models.Model):
 
 
-@eel.expose
-def my_add(a, b):
-    return a+b
+    """
+    """
+name = models.CharField(_('name'), max_length=255)
+is_template = models.BooleanField(default=False)
+status = models.CharField(max_length=30)
+flow = models.OneToOneField(ProcessTaskSet)
+
+
+
+def __unicode__(self):
+    return self.name
+
+
+class Meta:
+    verbose_name = _("process")
